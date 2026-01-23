6cff52a10421b3c6d52b28e2680347709be19f44
emrecan-aycil <emrecanaycil@icloud.com>
2026-01-23 18:05:45 +0300

    Add files via upload
    Add problem set 0 solution

---
+++ ps0.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+MIT 6.0001 Problem Set 0
+"""
+
+import numpy
+
+x = int(input("Enter number x:"))
+
+y = int(input("Enter number y:"))
+
+print("x**y =", x**y)
+
+print("log(x) =", int (numpy.log2(x)))
